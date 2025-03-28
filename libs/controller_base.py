0c840fc8c68021941471afeef415e7c5e95b3842
lupin <steffenlorenz@hotmail.de>
2025-03-28 11:22:16 +0100

    refactor to prevent duplicated code

--- libs/controller_base.py
+++ libs/controller_base.py
@@ -1,5 +1,13 @@
+from logging import getLogger, info
 from abc import ABC, abstractmethod
+from atexit import register
+from configparser import ConfigParser
+from pathlib import Path
+from threading import Thread, Lock
 from typing import Dict
+
+
+logger = getLogger(__name__)
 
 
 class BaseController(ABC):
@@ -11,6 +19,79 @@ class BaseController(ABC):
     """
 
     _REQUIRED_SECTIONS = ['Identification', 'Buttons', 'AnalogSticks']
+
+    def __init__(self, file_name: str):
+        """
+        Represents a controller interface for handling configuration, connections, and
+        interaction with a controller device via hidapi.
+
+        :param file_name: The configuration file name for the controller.
+        :type file_name: str
+        """
+        self._configuration_file_name = str(file_name)
+        self._config = None
+        self._controller = None
+
+        self._load_controller_configuration()
+        self._connect_to_controller()
+        self._initialize_steering()
+
+        self._btn_status = {
+            'TAKEOFF': False,
+            'LANDING': False,
+            'PHOTO': False
+        }
+
+        self._analog_right_stick = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False
+        }
+
+        self._analog_left_stick = {
+            "up": False,
+            "down": False,
+            "clockwise": False,
+            "counterclockwise": False
+        }
+
+        register(self._close)
+
+        self._lock = Lock()
+        self._thread = Thread(target=self._read_controller, daemon=True)
+        self._thread.start()
+
+    def _load_controller_configuration(self) -> None:
+        """
+        Loads and initializes the controller configuration.
+
+        :raises FileNotFoundError: If the configuration file is not found.
+        :raises ValueError: If any required configuration sections are missing.
+        :return: None
+        """
+        info('Loading controller configuration.')
+        config_path = (Path(__file__).parent.parent / "config" / self._configuration_file_name)
+
+        if not config_path.exists():
+            raise FileNotFoundError(f'Configuration file "{config_path}" not found.')
+
+        config = ConfigParser(strict=True)
+        config.read(config_path)
+
+        for section in self._REQUIRED_SECTIONS:
+            if section not in config:
+                raise ValueError(f'Missing config section: {section}')
+
+        self._config = config
+
+    @abstractmethod
+    def _connect_to_controller(self) -> None:
+        pass
+
+    @abstractmethod
+    def _initialize_steering(self) -> None:
+        pass
 
     @abstractmethod
     def _close(self) -> None:
@@ -30,39 +111,38 @@ class BaseController(ABC):
         """
         pass
 
-    @abstractmethod
     def get_btn_status(self) -> Dict[str, bool]:
         """
-        Returns the current status of the mapped controller buttons.
+        Retrieves the current digital button status.
 
-        :return: A dictionary indicating the pressed state of each button.
+        :return: The current digital button status.
         :rtype: Dict[str, bool]
         """
-        pass
+        with self._lock:
+            return self._btn_status.copy()
 
-    @abstractmethod
     def get_analog_right_stick(self) -> Dict[str, bool]:
         """
-        Returns the current directional state of the right analog stick.
+        Retrieves the current state of the analog right stick.
 
-        :return: A dictionary with directional booleans (e.g., 'forward', 'left', etc.).
+        :return: The current state of the analog right stick.
         :rtype: Dict[str, bool]
         """
-        pass
+        with self._lock:
+            return self._analog_right_stick.copy()
 
-    @abstractmethod
     def get_analog_left_stick(self) -> Dict[str, bool]:
         """
-        Returns the current directional state of the left analog stick.
+        Retrieves the current state of the analog left stick.
 
-        :return: A dictionary with directional booleans (e.g., 'up', 'counterclockwise', etc.).
+        :return: The current state of the analog left stick.
         :rtype: Dict[str, bool]
         """
-        pass
+        with self._lock:
+            return self._analog_left_stick.copy()
 
-    @abstractmethod
     def __del__(self):
         """
         Ensures proper cleanup when the controller object is garbage-collected.
         """
-        pass
+        self._close()
--- libs/controller_hid.py
+++ libs/controller_hid.py
@@ -1,9 +1,5 @@
 from logging import getLogger, info, error
-from atexit import register
-from configparser import ConfigParser
-from pathlib import Path
 from sys import exit
-from threading import Thread, Lock
 from time import sleep
 from typing import Optional
 from hid import device
@@ -30,42 +26,7 @@ class HidController(BaseController):
         :param file_name: The configuration file name for the controller.
         :type file_name: str
         """
-        self._configuration_file_name = str(file_name)
-        self._config = None
-        self._controller = None
-
-        self._load_controller_configuration()
-        self._connect_to_controller()
-        self._initialize_steering()
-
-        register(self._close)
-
-        self._lock = Lock()
-        self._thread = Thread(target=self._read_controller, daemon=True)
-        self._thread.start()
-
-    def _load_controller_configuration(self) -> None:
-        """
-        Loads and initializes the controller configuration.
-
-        :raises FileNotFoundError: If the configuration file is not found.
-        :raises ValueError: If any required configuration sections are missing.
-        :return: None
-        """
-        info('Loading controller configuration.')
-        config_path = (Path(__file__).parent.parent / "config" / self._configuration_file_name)
-
-        if not config_path.exists():
-            raise FileNotFoundError(f'Configuration file "{config_path}" not found.')
-
-        config = ConfigParser(strict=True)
-        config.read(config_path)
-
-        for section in self._REQUIRED_SECTIONS:
-            if section not in config:
-                raise ValueError(f'Missing config section: {section}')
-
-        self._config = config
+        super().__init__(file_name)
 
     def _connect_to_controller(self) -> None:
         """
@@ -103,12 +64,6 @@ class HidController(BaseController):
         identification_section = self._config['Identification']
         self._report_length = int(identification_section['report_length'])
 
-        self._btn_status = {
-            'TAKEOFF': False,
-            'LANDING': False,
-            'PHOTO': False
-        }
-
         btn_section = self._config['Buttons']
         self._btn_byte_index = int(btn_section['btn_byte_index'])
 
@@ -116,20 +71,6 @@ class HidController(BaseController):
             'TAKEOFF': int(btn_section['btn_takeoff_value']),
             'LANDING': int(btn_section['btn_landing_value']),
             'PHOTO': int(btn_section['btn_photo_value'])
-        }
-
-        self._analog_right_stick = {
-            "forward": False,
-            "backward": False,
-            "left": False,
-            "right": False
-        }
-
-        self._analog_left_stick = {
-            "up": False,
-            "down": False,
-            "clockwise": False,
-            "counterclockwise": False
         }
 
         analog_section = self._config['AnalogSticks']
@@ -233,39 +174,3 @@ class HidController(BaseController):
                             self._set_left_stick_active(None)
 
             sleep(self._DELAY)
-
-    def get_btn_status(self) -> dict:
-        """
-        Retrieves the current digital button status.
-
-        :return: The current digital button status.
-        :rtype: dict
-        """
-        with self._lock:
-            return self._btn_status.copy()
-
-    def get_analog_right_stick(self) -> dict:
-        """
-        Retrieves the current state of the analog right stick.
-
-        :return: The current state of the analog right stick.
-        :rtype: dict
-        """
-        with self._lock:
-            return self._analog_right_stick.copy()
-
-    def get_analog_left_stick(self) -> dict:
-        """
-        Retrieves the current state of the analog left stick.
-
-        :return: The current state of the analog left stick.
-        :rtype: dict
-        """
-        with self._lock:
-            return self._analog_left_stick.copy()
-
-    def __del__(self):
-        """
-        Ensures resources are released properly.
-        """
-        self._close()
